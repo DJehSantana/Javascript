157223d3d8647d0a3815134a984f9370be943b47
DJehSantana <jessicah.dias23@hotmail.com>
2022-08-09 11:29:03 -0300

    instanciando produtos,  copiando função validarLista do exercício anterior

--- exercicios_POO/mercadoPOO/mercado.js
+++ exercicios_POO/mercadoPOO/mercado.js
@@ -7,3 +7,38 @@ const leitor = readline.createInterface({
     output: process.stdout
 });
 
+const produtosMercado = [
+    new Produto ("arroz", 18.50),
+    new Produto ("feijao", 7),
+    new Produto ("melancia", 5),
+    new Produto ("alface", 2),
+    new Produto ("suco", 12)
+
+];
+
+
+//validando a lista
+const validarLista = (listaMercado) => {
+    //operador ! verifica se a lista está vazia: false, null ou undefined
+    if (!listaMercado) {
+        //throw : lança um erro e para a execução do escopo
+        throw Error ("A lista não pode ser vazia!");
+    }
+    //transformando string de itens digitados pelo usuário em um array
+    // o método split determina um separador para dividir uma string em 
+    //índices de um array. Neste caso a vírgula será o delimitador
+    const itensDesejados = listaMercado.split(","); 
+    //filtrando itens vazios
+    //o método trim remove espaços vazios antes e depois da string
+    const itensInvalidos = itensDesejados.filter(item => !item.trim()).length;
+
+    //tratamento de exceção para lista de itens invalidos
+    if (itensInvalidos > 0) {
+        throw Error (`Existem ${itensInvalidos} itens inválidos!`);
+    }
+
+    //retornando o array de itens
+    return itensDesejados;
+
+}
+
