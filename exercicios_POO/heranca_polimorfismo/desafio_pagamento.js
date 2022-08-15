b8395c6d736b9d8a46c734a8ea03b343aedac060
DJehSantana <jessicah.dias23@hotmail.com>
2022-08-15 11:01:54 -0300

    validando valores dos parametros

--- exercicios_POO/heranca_polimorfismo/desafio_pagamento.js
+++ exercicios_POO/heranca_polimorfismo/desafio_pagamento.js
@@ -1,4 +1,12 @@
 //receber a forma de pagamento e o valor
+const forma_pagamento = process.argv[2];
+const valor = process.argv[3];
+
 //validar os parametros de entrada
+if(!forma_pagamento || !valor) {
+    console.log('Forma de pagamento ou valor inválidos!');
+}
+
 //identificar a forma de pagamento
 //efetuar o pagamento
+
